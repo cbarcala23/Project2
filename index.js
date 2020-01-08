8e9c80feb3a0cfa5920f2d844c47df191780e80c
itadmin <itadmin@class.local>
2020-01-07 21:11:14 -0800

    chat updated

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/lounge.html');
 });
+
+var roomno = 1;
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+    // socket.join("room-"+roomno);
+    // //Send this event to everyone in the room.
+    // io.sockets.in("room-"+roomno).emit('connectToRoom', "You are in room no. "+roomno);
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
@@ -18,6 +23,7 @@ io.on('connection', function (socket) {
         io.emit('chat message', msg);
     });
 });
+
 
 
 http.listen(3000, function () {
