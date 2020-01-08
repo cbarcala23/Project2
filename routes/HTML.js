e6389e4d16b4f7a07349719e2ac20ab0a0e45fa8
BSmalls6 <smallwoodbrennan@gmail.com>
2020-01-07 21:19:27 -0800

    html routes

--- routes/HTML.js
+++ routes/HTML.js
@@ -1,9 +1,26 @@
 var db = require("../models");
 module.exports = function(app) {
 
-
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../main.html"));
+      });
+    
+      // cms route loads cms.html
+      app.get("/lounge", function(req, res) {
+        res.sendFile(path.join(__dirname, "../lounge.html"));
+      });
+    
+      // blog route loads blog.html
+    //   app.get("/blog", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/blog.html"));
+    //   });
+    
+    //   // authors route loads author-manager.html
+    //   app.get("/authors", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+    //   });
 
 
 
     
-}
+};
